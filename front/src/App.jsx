c312ec236edb1a3d2eff0ce23ff4b8d498f2d3c5
ali <alizreik1810@gmail.com>
2023-09-13 12:12:09 +0300

    institutionlogin

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,8 @@ import {Routes,Route,Outlet} from 'react-router-dom'
 import Home from "./Components/HOME/Home";
 import Navbar from "./Components/NAVBAR/Navbar";
 import About from "./Components/ABOUT/About";
+import Institutionlogin from "./Components/InstitutionLogin/Institutionlogin";
+import InstituteHome from "./Components/InstitutionLogin/InstituteHome";
 
 
 const App = () => {
@@ -12,6 +14,9 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Home/>}></Route>
             <Route path="/about" element={<About/>}></Route>
+            <Route path="/Institutionlogin" element={<Institutionlogin/>}></Route>
+            <Route path="/InstituteHome" element={<InstituteHome/>}></Route>
+
         </Routes>
         </>
     )
