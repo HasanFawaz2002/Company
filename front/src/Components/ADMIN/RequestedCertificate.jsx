9f775952d2eea491c72eeabbff7a3667f62af634
unknown <hasan.amaz2002@gmail.com>
2023-09-16 16:45:48 +0300

    Front

--- front/src/Components/ADMIN/RequestedCertificate.jsx
+++ front/src/Components/ADMIN/RequestedCertificate.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
 import './RequestedCertificate.css';
+import image from '../../images/image1.png'
 
 function RequestedCertificate() {
   return (
-    <div>RequestedCertificate</div>
+    <>
+    <div className='requested-certificate-container'>
+        <div class="requested-ceritificate-card" >
+                <div className="img-box">
+                    <img src={image}/>
+                </div>
+                <div class="content">
+                    <h2>Certificate name</h2>
+                    <div className="list">
+                    
+                    <li>
+                        <strong>_Requested By:</strong> Hasan
+                    </li>
+                    <li>
+                       <strong>_Student email:</strong> hasan.f2002@gmail.com
+                    </li>
+                    <li>
+                       <strong>_Status:</strong><span>Accepted</span>
+                    </li>
+                    </div>
+                    
+                    <button className='first-button'>Read More</button>
+                    <button className='second-button'>Read More</button>
+                </div>
+            </div>
+            <div class="requested-ceritificate-card" >
+                <div className="img-box">
+                    <img src={image}/>
+                </div>
+                <div class="content">
+                    <h2>Certificate name</h2>
+                    <div className="list">
+                    
+                    <li>
+                        <strong>_Requested By:</strong> Hasan
+                    </li>
+                    <li>
+                       <strong>_Student email:</strong> hasan.f2002@gmail.com
+                    </li>
+                    <li>
+                       <strong>_Status:</strong><span>Accepted</span>
+                    </li>
+                    </div>
+                    
+                    <button className='first-button'>Read More</button>
+                    <button className='second-button'>Read More</button>
+                </div>
+            </div>
+            <div class="requested-ceritificate-card" >
+                <div className="img-box">
+                    <img src={image}/>
+                </div>
+                <div class="content">
+                    <h2>Certificate name</h2>
+                    <div className="list">
+                    
+                    <li>
+                        <strong>_Requested By:</strong> Hasan
+                    </li>
+                    <li>
+                       <strong>_Student email:</strong> hasan.f2002@gmail.com
+                    </li>
+                    <li>
+                       <strong>_Status:</strong><span>Accepted</span>
+                    </li>
+                    </div>
+                    
+                    <button className='first-button'>Read More</button>
+                    <button className='second-button'>Read More</button>
+                </div>
+            </div>
+            
+           
+            
+           
+            
+            
+            
+    </div>
+    </>
   )
 }
 
--- front/src/Components/ADMIN/Sidebar.jsx
+++ front/src/Components/ADMIN/Sidebar.jsx
@@ -1,59 +1,71 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import 
-{BsCart3, BsGrid1X2Fill, BsFillPlusSquareFill, BsFileEarmarkPlusFill, BsFileEarmarkText, 
-    BsFileEarmarkCheck, BsMenuButtonWideFill, BsFillGearFill}
- from 'react-icons/bs'
+import {
+  BsGrid1X2Fill,
+  BsFillPlusSquareFill,
+  BsFileEarmarkPlusFill,
+  BsFileEarmarkText,
+  BsFileEarmarkCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from 'react-icons/bs';
+import logo from '../../images/logo.png';
 
-function Sidebar({openSidebarToggle, OpenSidebar}) {
+function Sidebar({ openSidebarToggle, OpenSidebar }) {
+  const closeSidebar = () => {
+    if (openSidebarToggle) {
+      OpenSidebar();
+    }
+  };
+
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-                <BsCart3  className='icon_header'/> Zidyia
-            </div>
-            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""}>
+      <div className='sidebar-title'>
+        <div className='sidebar-brand'>
+          <img src={logo} alt="" /> Zidyia
         </div>
+        <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+      </div>
 
-        <ul className='sidebar-list'>
-            <li className='sidebar-list-item'>
-                <NavLink to="/admin">
-                    <BsGrid1X2Fill className='icon'/> Dashboard
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-               <NavLink to="/admin/createcertificate">
-                    <BsFillPlusSquareFill className='icon'/> Add Certificate
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to="/admin/CustomizableForm">
-                    <BsFileEarmarkPlusFill className='icon'/> Add Request Form
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to="/admin/RequestedCertificate">
-                    <BsFileEarmarkText className='icon'/> Requested Certificate
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to="/admin/UploadedCertificate">
-                    <BsFileEarmarkCheck className='icon'/> Uploaded Certificate
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsMenuButtonWideFill className='icon'/> Reports
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGearFill className='icon'/> Setting
-                </a>
-            </li>
-        </ul>
+      <ul className='sidebar-list'>
+        <li className='sidebar-list-item'>
+          <NavLink to="/admin" onClick={closeSidebar}>
+            <BsGrid1X2Fill className='icon' /> Dashboard
+          </NavLink>
+        </li>
+        <li className='sidebar-list-item'>
+          <NavLink to="/admin/createcertificate" onClick={closeSidebar}>
+            <BsFillPlusSquareFill className='icon' /> Add Certificate
+          </NavLink>
+        </li>
+        <li className='sidebar-list-item'>
+          <NavLink to="/admin/CustomizableForm" onClick={closeSidebar}>
+            <BsFileEarmarkPlusFill className='icon' /> Add Request Form
+          </NavLink>
+        </li>
+        <li className='sidebar-list-item'>
+          <NavLink to="/admin/RequestedCertificate" onClick={closeSidebar}>
+            <BsFileEarmarkText className='icon' /> Requested Certificate
+          </NavLink>
+        </li>
+        <li className='sidebar-list-item'>
+          <NavLink to="/admin/UploadedCertificate" onClick={closeSidebar}>
+            <BsFileEarmarkCheck className='icon' /> Uploaded Certificate
+          </NavLink>
+        </li>
+        <li className='sidebar-list-item'>
+          <a href="" onClick={closeSidebar}>
+            <BsMenuButtonWideFill className='icon' /> Reports
+          </a>
+        </li>
+        <li className='sidebar-list-item'>
+          <a href="" onClick={closeSidebar}>
+            <BsFillGearFill className='icon' /> Setting
+          </a>
+        </li>
+      </ul>
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
